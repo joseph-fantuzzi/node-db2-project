b5b4cf4cdea2524b98997c97155327ada3ff7a94
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-19 15:57:52 -0400

    get all request and error handling in place

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,14 @@
-// DO YOUR MAGIC
+const router = require("express").Router();
+
+const Cars = require("./cars-model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.status(200).json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
+const CarsRouter = require("./cars/cars-router");
 
-// DO YOUR MAGIC
+const server = express();
 
-module.exports = server
+server.use(express.json());
+
+server.use("/api/cars", CarsRouter);
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
