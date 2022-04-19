fec8201f9c1f94ebf0db9271652beaa698ca870a
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-19 15:37:30 -0400

    migration and seed files in place

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
 // STRETCH
+
+const initialCars = [
+  {
+    vin: "12345",
+    make: "Honda",
+    model: "Civic",
+    mileage: 65600,
+    title: "clean",
+    transmission: "automatic",
+  },
+  {
+    vin: "67891",
+    make: "Audi",
+    model: "R8",
+    mileage: 1000,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "25432",
+    make: "Tesla",
+    model: "Model S Plaid",
+    mileage: 5000,
+    title: "clean",
+    transmission: "automatic",
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(initialCars);
+};
